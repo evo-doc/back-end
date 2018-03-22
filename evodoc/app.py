00f9949c50e068c15232a2de1c64113862ac3c71
Tomas Stary <tomas.stary@icloud.com>
2018-03-22 12:13:55 +0100

    Adding migration handler, added first migration, edited almost everything

--- evodoc/app.py
+++ evodoc/app.py
@@ -1,28 +1,14 @@
 import os
 
 from flask import Flask
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('evodoc.appsettings.AppSettings')
 app.config.from_pyfile(os.path.dirname(__file__) + '/../conf/appsettings.local.ini')
 
+db = SQLAlchemy(app)
+from evodoc.entity.models import User, UserType
 
-engine = create_engine(app.config["DATABASE_CONNECTION"])
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-
-Base = declarative_base()
-Base.query = db_session.query_property()
-
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import evodoc.models
-    Base.metadata.create_all(bind=engine)
+migrate = Migrate(app, db)
--- evodoc/entity/models.py
+++ evodoc/entity/models.py
@@ -1,16 +1,37 @@
-from sqlalchemy import Column, Integer, String
-from evodoc.app import Base
+import datetime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from evodoc.app import db
 
-# TODO: better user asshole
-class User(Base):
-    __tablename__ = 'users'
+class User(db.Model):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
+    user_type_id =Column(Integer, ForeignKey("user_type.id"))
     name = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
+    password = Column(String(128))
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    update = Column(DateTime, default=datetime.datetime.utcnow)
+    active = Column(Boolean)
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, email=None, password=None, created=None, active=True):
         self.name = name
         self.email = email
+        self.password = password
+        self.created = created
+        self.active = active
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return "<User %r>" % (self.name)
+
+class UserType(db.Model):
+    __tablename__ = "user_type"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    permission_flag = Column(Integer)
+
+    def __init__(self, name=None, permission_flag=0):
+        self.name = name
+        self.permission_flag = permission_flag
+
+    def __repr__(self):
+        return "<UserType %r>" % (self.name)
