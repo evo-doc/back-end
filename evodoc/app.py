5ba8e8dc200c232f8c6a3aefdaa8f8f1083d3ddb
Daniel Hampl <danhampl31415@gmail.com>
2018-04-06 08:56:29 -0400

    should work ;)

--- evodoc/app.py
+++ evodoc/app.py
@@ -9,7 +9,10 @@ app.config.from_object('evodoc.appsettings.AppSettings')
 app.config.from_pyfile(os.path.dirname(__file__) + '/../conf/appsettings.local.ini')
 
 db = SQLAlchemy(app)
-from evodoc.entity.models import User, UserType
+#engine = create_engine("postgres://")
+#session_factory = sessionmaker(bind=db)
+
+from evodoc.entity.models import User, UserType, UserToken
 
 migrate = Migrate(app, db)
 #perform upgrade
@@ -19,4 +22,10 @@ with app.app_context():
 
 from evodoc.entity.seed.userseed import *
 
-userInsert()
+from evodoc.login import *
+
+@app.route('/')
+def home():
+	return "hillo wrld"
+
+#userInsert()
--- evodoc/login.py
+++ evodoc/login.py
@@ -1,42 +1,55 @@
 #needs to load connection from default file
+from evodoc.app import *
 #needs import UserToken
-import os
+#import os
 
 #from flask import Flask
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate
 
-from sqlalchemy.engine import create_engine
-from sqlalchemy import *
+#from sqlalchemy.engine import create_engine
+#from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.ext.declarative import declarative_base
 #login = Flask(__name__, instance_relative_config=True)
 #login.config.from_object('evodoc.appsettings.AppSettings')
 #login.config.from_pyfile(os.path.dirname(__file__) + '/../conf/appsettings.local.ini')
 #from evodoc.entity.models import User, UserType, UserToken
 import uuid
+from flask_sqlalchemy_session import flask_scoped_session
+from datetime import 	datetime, timedelta
 
-db = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+#db = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
 
-Session = sessionmaker(bind=db)
-session = Session()
-Base = declarative_base()
 
-def createToken (userId) :
-	token = str(userId).zfill(10) + str(uuid.uuid4())
-	while session.query(UserToken).filter(UserToken.token=token).count() != 0 :
-		token = str(userId).zfill(10) + str(uuid.uuid4())
-	return token
+
+#Session = sessionmaker(bind=db)
+#session = Session()
+#Base = declarative_base()
+
+def createToken (userId) : #creates new token and adds it to the database
+	session = db.create_scoped_session(options=None)
+	t = str(userId).zfill(10) + str(uuid.uuid4())
+	while (session.query(UserToken).filter_by(token=t).count() != 0) :
+		t = str(userId).zfill(10) + str(uuid.uuid4())
+	session.add(UserToken(user_id=userId,token=t))
+	session.commit()
+	return t
 	
-def authenticateUser (id, token):
-	for token in session.query(UserToken).filter(UserToken.userId=id).all():
-		print(token.token)
-		#check if it has active token
-		#if so give it back
-		#otherwise createToken(id)
-	
+def authenticateUser (id, token=None): #returns active token
+	if (token==None) :
+		return createToken(id)
+	session = db.create_scoped_session(options=None)
+	#make sure the token is active
+	for token in session.query(UserToken).filter(UserToken.user_id==id, UserToken.created +  timedelta(hours=24) > datetime.utcnow(), UserToken.update +  timedelta(hours=2) > datetime.utcnow()):
+		token.update=datetime.utcnow()#if token is active update it
+		session.commit()
+		session.close()
+		return token.token
+	#otherwise createToken(id)
+	return createToken(id)
 
 
-
-token = createToken(123456789)
-print(token)
+authenticateUser(0,0)
+#token = createToken(123456789)
+#print(token)
