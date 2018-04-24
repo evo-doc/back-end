2a2fa6198c36807efdc26730de9dea099b8a8588
Daniel Hampl <danhampl31415@gmail.com>
2018-04-24 11:10:39 +0200

    tabs to spaces

--- evodoc/login.py
+++ evodoc/login.py
@@ -7,46 +7,46 @@ from datetime import datetime, timedelta
 from evodoc.entity import *
 
 def login(username, password_plain):
-	try:
-		user = User.get_user_by_username_or_email(User, username)
-		if user.activated == False:
-			raise DbException(304, "User is not activated yet")
-		if (user.confirm_password(password_plain)):
-			return authenticateUser(user.id, None)
-	except DbException as err:
-			raise DbException(403, "Invalid username or password")
+    try:
+        user = User.get_user_by_username_or_email(User, username)
+        if user.activated == False:
+            raise DbException(304, "User is not activated yet")
+        if (user.confirm_password(password_plain)):
+            return authenticateUser(user.id, None)
+    except DbException as err:
+        raise DbException(403, "Invalid username or password")
 
 def createToken (userId) : #creates new token and adds it to the database
-	t = str(userId).zfill(10) + str(uuid.uuid4())
-	while (UserToken.query.filter_by(token=t).count() != 0) :
-		t = str(userId).zfill(10) + str(uuid.uuid4())
-	db.session.add(UserToken(user_id=userId,token=t))
-	db.session.commit()
-	return t
+    t = str(userId).zfill(10) + str(uuid.uuid4())
+    while (UserToken.query.filter_by(token=t).count() != 0) :
+        t = str(userId).zfill(10) + str(uuid.uuid4())
+    db.session.add(UserToken(user_id=userId,token=t))
+    db.session.commit()
+    return t
 
 def authenticateUser (id, token=None): #returns active token
-	if (token==None) :
-		return createToken(id)
-	#make sure the token is active
-	for token in UserToken.query.filter(UserToken.user_id==id, UserToken.created +  timedelta(hours=24) > datetime.utcnow(), UserToken.update +  timedelta(hours=2) > datetime.utcnow()):
-		token.update=datetime.utcnow()#if token is active update it
-		t=token.token
-		db.session.commit()
+    if (token==None) :
+        return createToken(id)
+    #make sure the token is active
+    for token in UserToken.query.filter(UserToken.user_id==id, UserToken.created +  timedelta(hours=24) > datetime.utcnow(), UserToken.update +  timedelta(hours=2) > datetime.utcnow()):
+        token.update=datetime.utcnow()#if token is active update it
+        t=token.token
+        db.session.commit()
 
-		return t
-	#otherwise createToken(id)
-	return createToken(id)
+        return t
+    #otherwise createToken(id)
+    return createToken(id)
 
 def authenticate(token):
-	"""
-	Test if token exist, if not returns None, if its out of date, returns new token, else return old one
-		:param token: user token
-	"""
-	if token == None:
-		return None
-	userTokenEntity = UserToken.query.filter(UserToken.token == token).first()
-	if userTokenEntity == None:
-		return None
-	if userTokenEntity.user.active != 1:
-		return None
-	return UserToken
+    """
+    Test if token exist, if not returns None, if its out of date, returns new token, else return old one
+        :param token: user token
+    """
+    if token == None:
+        return None
+    userTokenEntity = UserToken.query.filter(UserToken.token == token).first()
+    if userTokenEntity == None:
+        return None
+    if userTokenEntity.user.active != 1:
+        return None
+    return UserToken
