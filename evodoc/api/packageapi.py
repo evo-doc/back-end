2bd29c494ff7d83881ea526112c19006c59204b7
Tomas Stary <staryto5@fit.cvut.cz>
2018-05-06 19:25:11 +0200

    error handlers - minor refactor userapi.py

--- evodoc/api/packageapi.py
+++ evodoc/api/packageapi.py
@@ -1,7 +1,7 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, jsonify
 from evodoc import DbException, ApiException
 from evodoc.entity import Package
-from evodoc.api import response_ok, response_ok_obj, response_ok_list, response_err, validate_token, validate_data
+from evodoc.api import response_ok, response_ok_obj, response_ok_list, validate_token, validate_data
 
 package = Blueprint('package', __name__, url_prefix='/package')
 
@@ -10,74 +10,58 @@ def get_package_by_id_action():
     """
     Get packge by id
     """
-    try:
-        token = request.args.get('token')
-        validate_token(token)
-        package_id = request.args.get('package_id')
-        data = Package.get_package_by_id(package_id)
-        return response_ok_obj(data)
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    token = request.args.get('token')
+    validate_token(token)
+    package_id = request.args.get('package_id')
+    data = Package.get_package_by_id(package_id)
+    return response_ok_obj(data)
 
 @package.route('/all', methods=['GET'])
 def get_all_packages():
     """
     Return all packages
     """
-    try:
-        token = request.args.get('token')
-        validate_token(token)
-        return response_ok_list(Package.get_all_packages())
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    token = request.args.get('token')
+    validate_token(token)
+    return response_ok_list(Package.get_all_packages())
 
 @package.route('', methods=['POST'])
 def save_package():
-    try:
-        data = request.get_json()
-        validate_data(data, {'token'})
-        validate_token(data['token'])
-        package = Package.save_or_create(data)
-        return response_ok_obj(package)
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    """
+    Saves package (create or update)
+    """
+    data = request.get_json()
+    validate_data(data, {'token'})
+    validate_token(data['token'])
+    package = Package.save_or_create(data)
+    return response_ok_obj(package)
 
 @package.route('/down', methods=['POST'])
 def download_package():
-    try:
-        data = request.get_json()
-        validate_data(data, {'token', 'package_id'})
-        package = Package.get_package_by_id(data['package_id'])
-        package.download_package()
-        return response_ok({'data': 'Done'})
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    """
+    Tries to retrieve package from git
+    """
+    data = request.get_json()
+    validate_data(data, {'token', 'package_id'})
+    package = Package.get_package_by_id(data['package_id'])
+    package.download_package()
+    return response_ok({'data': 'Done'})
 
 @package.route('', methods=['DELETE'])
 def delete_package():
     """
     Deletes package
     """
-    try:
-        data = request.get_json()
-        validate_data(data, {'token', 'package_id'})
-        package = Package.get_package_by_id(data['package_id'])
-        package.delete_package()
-        data = {
-            "data": "done"
-        }
-        return response_ok(data)
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    data = request.get_json()
+    validate_data(data, {'token', 'package_id'})
+    package = Package.get_package_by_id(data['package_id'])
+    package.delete_package()
+    data = {
+        "data": "done"
+    }
+    return response_ok(data)
 
-
+@package.errorhandler(ApiException)
+@package.errorhandler(DbException)
+def __response_err(data):
+    return jsonify(data.message), data.errorCode
--- evodoc/api/projectapi.py
+++ evodoc/api/projectapi.py
@@ -1,7 +1,7 @@
-from flask import json, request, Blueprint
+from flask import json, request, Blueprint, jsonify
 from evodoc.exception import DbException, ApiException
 from evodoc.entity import *
-from evodoc.api import response_ok, response_err, response_ok_list, response_ok_obj, validate_token
+from evodoc.api import response_ok, response_ok_list, response_ok_obj, validate_token
 
 project = Blueprint('project', __name__, url_prefix='/project')
 
@@ -11,21 +11,16 @@ def get_project_by_id_action(id):
     Get project data by it's id
         :param id:
     """
-    try:
-        data = request.get_json()
-        if data == None or data == {}:
-            return response_err(ApiException(404, "No data suplied"))
-        if ('token' not in data) or (data['token'] == None):
-            raise ApiException(403, "Invalid token")
-        token = data['token']
-        validate_token(token)
-        #check permissions in the future
-        data = Project.get_project_by_id(Project, id)
-        return response_ok_obj(data)
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    data = request.get_json()
+    if data == None or data == {}:
+        raise ApiException(404, "No data suplied")
+    if ('token' not in data) or (data['token'] == None):
+        raise ApiException(403, "Invalid token")
+    token = data['token']
+    validate_token(token)
+    #check permissions in the future
+    data = Project.get_project_by_id(Project, id)
+    return response_ok_obj(data)
 
 @project.route('/name/<name>', methods=['GET'])
 def get_project_by_name_action(name):
@@ -33,68 +28,57 @@ def get_project_by_name_action(name):
     Get project data by it's name
         :param name: Project name
     """
-    try:
-        data = request.get_json()
-        if data == None or data == {}:
-            return response_err(ApiException(404, "No data suplied"))
-        if ('token' not in data) or (data['token'] == None):
-            raise ApiException(403, "Invalid token")
-        token = data['token']
-        validate_token(token)
-        #check permissions in the future
-        data = Project.get_project_by_name(Project, name)
-        if (data == None):
-            return response_err(ApiException(400, "Name already in use."))
-        return response_ok_obj(data)
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    data = request.get_json()
+    if data == None or data == {}:
+        raise ApiException(404, "No data suplied")
+    if ('token' not in data) or (data['token'] == None):
+        raise ApiException(403, "Invalid token")
+    token = data['token']
+    validate_token(token)
+    #check permissions in the future
+    data = Project.get_project_by_name(Project, name)
+    if (data == None):
+        return response_err(ApiException(400, "Name already in use."))
+    return response_ok_obj(data)
 
 @project.route('/all', methods=['GET'])
 def get_project_all_action():
     """
     Get data for all projects
     """
-    try:
-        data = request.get_json()
-        if data == None or data == {}:
-            return response_err(ApiException(404, "No data suplied"))
-        if ('token' not in data) or (data['token'] == None):
-            raise ApiException(403, "Invalid token")
-        token = data['token']
-        validate_token(token)
-        #check permissions in the future
-        data = Project.get_project_all()
-        return response_ok_list(data)
-    except DbException as err:
-        return response_err(err)
-    except ApiException as err:
-        return response_err(err)
+    data = request.get_json()
+    if data == None or data == {}:
+        raise ApiException(404, "No data suplied")
+    if ('token' not in data) or (data['token'] == None):
+        raise ApiException(403, "Invalid token")
+    token = data['token']
+    validate_token(token)
+    #check permissions in the future
+    data = Project.get_project_all()
+    return response_ok_list(data)
 
 @project.route("/update_or_create", methods=['POST'])
 def update_or_create_poject_action():
     """
     Update or create poject
     """
-    try:
-        data = request.get_json()
-        if data == None:
-            raise ApiException(400, "data")
-        if (data['token'] == None):
-            err = ApiException(403, "Invalid token")
-            return response_err(err)
-        if (('poject_id' not in data) or (data['poject_id'] == None)):
-            poject_id = None
-        else:
-            poject_id = data['poject_id']
-        validate_token(data['token'])
-        #check permissions in the future
-        data = Project.create_or_update_project_by_id_array(poject_id, data['data'], True)
-        if (data == None):
-            return response_err(ApiException(400, "Name already in use."))
-        return response_ok_obj(data)
-    except ApiException as err:
-        return response_err(err)
-    except DbException as err:
-        return response_err(err)
+    data = request.get_json()
+    if data == None:
+        raise ApiException(400, "data")
+    if (data['token'] == None):
+        raise ApiException(403, "Invalid token")
+    if (('poject_id' not in data) or (data['poject_id'] == None)):
+        poject_id = None
+    else:
+        poject_id = data['poject_id']
+    validate_token(data['token'])
+    #check permissions in the future
+    data = Project.create_or_update_project_by_id_array(poject_id, data['data'], True)
+    if (data == None):
+        raise ApiException(400, "Name already in use.")
+    return response_ok_obj(data)
+
+@project.errorhandler(ApiException)
+@project.errorhandler(DbException)
+def __response_err(data):
+    return jsonify(data.message), data.errorCode
